7e56c492051c56611af52b68e98155a6ebfea39c
keith <keithzhang021@outlook.com>
2022-09-15 18:35:48 -0400

    updated normal status for further use

--- status.py
+++ status.py
@@ -21,12 +21,15 @@ BLUE = Status("Sturdy", "blue")
 PURPLE = Status("Primed", "purple")
 GREEN = Status("忘了", "green")
 
+BLACK = Status("Poor", "black")
+RAINBOW = Status("Excellent", "rainbow")
+
 
 class StatusManager:
     BASIC = 0
     NORMAL = 1
     HARD = 2
-    NORMAL_STATUS = [WHITE, RED]
+    NORMAL_STATUS = [WHITE, RED, BLACK, RAINBOW]
     HARD_STATUS = [WHITE, RED, YELLOW, CYAN, BLUE, PURPLE]
 
     def __init__(self, mode=2):
@@ -34,17 +37,25 @@ class StatusManager:
 
         :param mode: Crafting mode
         """
+        self.last = WHITE
         self.mode = mode
 
     def next_status(self):
         if self.mode == 0:
             return WHITE
         elif self.mode == 1:
-            # 20% for red
-            if random.randint(0, 100) > 20:
-                return WHITE
+            next_status = None
+            if self.last == RAINBOW:
+                next_status = BLACK
+            elif random.randint(0, 100) < 4:
+                next_status = RAINBOW
+            elif random.randint(0, 100) < 20:
+                # 20% for red
+                next_status = RED
             else:
-                return RED
+                next_status = WHITE
+            self.last = next_status
+            return next_status
         elif self.mode == 2:
             # Evenly distributed TODO: need some data
             r = math.floor(random.random() * len(StatusManager.HARD_STATUS))
